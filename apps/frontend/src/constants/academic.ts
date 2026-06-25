70aa29b14be9c74bee250785c101d883a897d505
rislasherin <rislasherin09@gmail.com>
2026-06-25 12:30:29 +0530

    fix(Constants): Update system constants in academic.ts

---
+++ apps/frontend/src/constants/academic.ts
@@ -0,0 +1,23 @@
+export const PLATFORM_DEGREES = [
+  "B.Tech",
+  "M.Tech",
+  "BCA",
+  "MCA",
+  "B.Sc",
+  "M.Sc",
+  "Any Degree"
+];
+
+export const PLATFORM_BRANCHES = [
+  "Computer Science and Engineering (CSE)",
+  "Information Technology (IT)",
+  "Artificial Intelligence and Machine Learning (AI & ML)",
+  "Artificial Intelligence and Data Science (AI & DS)",
+  "Computer Science (Cyber Security)",
+  "Computer Science (Data Science)",
+  "Computer Science (IoT)",
+  "Software Engineering",
+  "Computer Applications (BCA/MCA)",
+  "Electronics and Communication Engineering (ECE)",
+  "Any IT Branch"
+];
