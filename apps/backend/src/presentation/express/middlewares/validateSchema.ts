a28e466231228155ed68554e1c3de6974f753c39
rislasherin <rislasherin09@gmail.com>
2026-06-25 12:29:56 +0530

    fix(Core): Update validateSchema.ts

--- apps/backend/src/presentation/express/middlewares/validateSchema.ts
+++ apps/backend/src/presentation/express/middlewares/validateSchema.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { AnyZodObject, ZodError } from "zod";
+import { HttpStatus } from "@domain/enums/HttpStatus.enum";
 
 export const validateSchema = (schema: AnyZodObject) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -8,7 +9,7 @@ export const validateSchema = (schema: AnyZodObject) => {
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        return res.status(400).json({
+        return res.status(HttpStatus.BAD_REQUEST).json({
           status: 'error',
           message: 'Validation failed',
           errors: error.errors.map(err => ({
