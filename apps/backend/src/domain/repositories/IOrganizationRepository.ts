324b9b91cc283cad7641ddbc38434fd233aa985a
rislasherin <rislasherin09@gmail.com>
2026-06-25 12:29:44 +0530

    fix(Core): Update IOrganizationRepository.ts

--- apps/backend/src/domain/repositories/IOrganizationRepository.ts
+++ apps/backend/src/domain/repositories/IOrganizationRepository.ts
@@ -3,6 +3,6 @@ import { Organization } from "@domain/entities/Organization";
 
 export interface IOrganizationRepository extends IBaseRepository<Organization> {
     findByName(name:string) :Promise<Organization | null>
-    searchOrganizations(query: string, page: number, limit: number): Promise<{ organizations: Organization[], total: number }>;
+    searchOrganizations(query: string, page: number, limit: number, status?: string): Promise<{ organizations: Organization[], total: number }>;
     updateStatus(id: string, status: string, blockedBy?: string): Promise<void>;
 }
