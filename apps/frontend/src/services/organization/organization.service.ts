880afb47bf6e77713e780acf7ca810b1220eae34
rislasherin <rislasherin09@gmail.com>
2026-06-25 12:30:20 +0530

    feat(Service): Update API service organization.service.ts

--- apps/frontend/src/services/organization/organization.service.ts
+++ apps/frontend/src/services/organization/organization.service.ts
@@ -3,7 +3,7 @@ import { apiClient } from '../api/api.client';
 
 export const OrganizationService = {
   getOrganizations: async (): Promise<{ id: string; name: string; activeBranches?: string[] }[]> => {
-    const response = (await apiClient.get(API_ROUTES.AUTH.ORGANIZATIONS_APPROVED)) as type;
+    const response = (await apiClient.get(API_ROUTES.AUTH.ORGANIZATIONS_APPROVED)) as any;
     return response.data || [];
   }
 };
